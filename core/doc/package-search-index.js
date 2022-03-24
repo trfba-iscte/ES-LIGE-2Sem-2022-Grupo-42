dea486f75bb274e226ebdef80732abb802357499
trfba-iscte <trfba@iscte-iul.pt>
2022-03-24 11:24:01 +0000

    Push master e javadoc

---
+++ core/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.google.zxing.datamatrix.detector"},{"l":"com.google.zxing.maxicode.decoder"},{"l":"com.google.zxing.multi.qrcode.detector"},{"l":"com.google.zxing.pdf417.detector"},{"l":"com.google.zxing.qrcode.detector"}];updateSearchResults();
